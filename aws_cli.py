20d2f5d1473a9a60abf0df72f49c6853c5629396
Jakub Praszkowski <kubapraszkowski@gmail.com>
2024-10-23 20:42:32 +0200

    Add: Delete files using regex

--- aws_cli.py
+++ aws_cli.py
@@ -5,8 +5,6 @@ import boto3
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-BUCKET_NAME = 'developer-task'
-PREFIX = 'a-wing/'
 
 s3_client = boto3.client(
     's3',
@@ -15,9 +13,9 @@ s3_client = boto3.client(
 )
 
 
-def list_files():
+def list_files(bucket_name, prefix):
     print("Listing files in bucket...")
-    response = s3_client.list_objects_v2(Bucket=BUCKET_NAME, Prefix=PREFIX)
+    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
     if 'Contents' in response:
         for obj in response['Contents']:
             print(obj['Key'])
@@ -25,12 +23,12 @@ def list_files():
         print("No files found.")
 
 
-def upload_file(local_file_path):
-    key = os.path.join(PREFIX, os.path.basename(local_file_path))
+def upload_file(bucket_name, prefix, local_file_path):
+    key = os.path.join(prefix, os.path.basename(local_file_path))
     
     try:
         print(f"Uploading {local_file_path} to {key}")
-        s3_client.upload_file(local_file_path, BUCKET_NAME, key)
+        s3_client.upload_file(local_file_path, bucket_name, key)
     
     except Exception as e:
         print(f"Failed to upload {local_file_path}: {e}")
@@ -39,10 +37,9 @@ def upload_file(local_file_path):
         print("Upload completed.")
 
 
-def list_files_with_filter(path, regex_pattern):
+def list_files_with_filter(bucket_name, prefix, regex_pattern):
     print(f"Listing files matching pattern: {regex_pattern}")
-    response = s3_client.list_objects_v2(Bucket=BUCKET_NAME, Prefix=PREFIX)
-    pattern = re.compile(regex_pattern)
+    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
     if 'Contents' in response:
         for obj in response['Contents']:
             if re.search(regex_pattern, obj['Key']):
@@ -51,11 +48,27 @@ def list_files_with_filter(path, regex_pattern):
         print("No files found.")
 
 
-def main():
-    # upload_file('README.md')
-    list_files()
-    list_files_with_filter('a-wing/', r'\.jpg$')
+def delete_files_matching_regex(bucket_name, prefix, regex_pattern):
+    print(f"Deleting files matching pattern: {regex_pattern}")
+    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+    pattern = re.compile(regex_pattern)
+    if 'Contents' in response:
+        for obj in response['Contents']:
+            if pattern.search(obj['Key']):
+                print(f"Deleting {obj['Key']}")
+                s3_client.delete_object(Bucket=bucket_name, Key=obj['Key'])
+    else:
+        print("No files found.")
 
+
+def main():
+    BUCKET_NAME = 'developer-task'
+    PREFIX = 'a-wing/'
+    
+    list_files(BUCKET_NAME, PREFIX)
+    # upload_file(BUCKET_NAME, PREFIX, 'a-wing/README.md')
+    delete_files_matching_regex(BUCKET_NAME, PREFIX, r'\.md$')
+    list_files_with_filter(BUCKET_NAME, PREFIX, r'\.md$')
 
 if __name__ == "__main__":
     main()

c49f5df4b843502daa8e48a88b58d1408e3d161b
Jakub Praszkowski <kubapraszkowski@gmail.com>
2024-10-23 20:32:45 +0200

    Add: List files using regex

--- aws_cli.py
+++ aws_cli.py
@@ -1,4 +1,5 @@
 import os
+import re
 import boto3
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -37,9 +38,23 @@ def upload_file(local_file_path):
     else:
         print("Upload completed.")
 
+
+def list_files_with_filter(path, regex_pattern):
+    print(f"Listing files matching pattern: {regex_pattern}")
+    response = s3_client.list_objects_v2(Bucket=BUCKET_NAME, Prefix=PREFIX)
+    pattern = re.compile(regex_pattern)
+    if 'Contents' in response:
+        for obj in response['Contents']:
+            if re.search(regex_pattern, obj['Key']):
+                print(obj['Key'])
+    else:
+        print("No files found.")
+
+
 def main():
-    upload_file('README.md')
+    # upload_file('README.md')
     list_files()
+    list_files_with_filter('a-wing/', r'\.jpg$')
 
 
 if __name__ == "__main__":
